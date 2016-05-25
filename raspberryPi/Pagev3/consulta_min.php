f1da7b9369d39112d3d9a7893267042083b96b97
rgborges <rafel430g@gmail.com>
2016-05-24 21:22:17 -0300

    Melhorias da página consulta php adaptado para um banco local no raspberry

---
+++ raspberryPi/Pagev3/consulta_min.php
@@ -0,0 +1,38 @@
+
+<?php 
+// definine  host, database, usuario e senha
+//$host = "mysql.hostinger.com.br";
+//$db   = "u765779207_simu";
+//$user = "u765779207_root";
+//$pass = "simuroot";
+$host = "localhost";
+$dbs = "iot";
+$user = "monty";
+$pass = "SENAI";
+//Conecta-se ao banco
+$db = new mysqli($host, $user, $pass, $dbs);
+//Verifica conexão
+if(mysqli_connect_errno())
+{
+  die('Não foi possível se conectar ao banco['.$db->connect_error.']');
+
+}
+
+$result = $db->query('SELECT * from iot.node;');
+
+echo '<TABLE BORDER="3">';
+echo PHP_EOL;
+
+while($func = $result->fetch_row())
+{
+ echo'<TR>';
+ foreach($func as $i => $value)
+{
+  echo '<TD>'.$value.'</TD>';
+}
+echo '</TR>';
+}
+echo'</TABLE>';
+$result->close();
+
+?>
